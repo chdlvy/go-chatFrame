86a497a84515be94cbaead312f3331271eb243cb
chdlvy <2583756066@qq.com>
2023-12-09 20:03:05 +0800

    chatFrame

--- go.mod
+++ go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/bwmarrin/snowflake v0.3.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/chdlvy/protocol v1.0.4 // indirect
+	github.com/chdlvy/protocol v1.0.5 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
--- internal/rpc/friend/friend.go
+++ internal/rpc/friend/friend.go
@@ -1,8 +1,12 @@
 package friend
 
 import (
+	"context"
 	"github.com/chdlvy/go-chatFrame/pkg/common/db"
 	"github.com/chdlvy/go-chatFrame/pkg/common/db/cache"
+	friendPb "github.com/chdlvy/protocol/friend"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 )
 
@@ -27,4 +31,49 @@ func Start() error {
 	//grpcServer := grpc.NewServer()
 	//grpcServer.RegisterService()
 	return friendServer
+}
+
+type FriendRPCServer struct {
+	friendPb.UnimplementedFriendServer
+}
+
+func (friendRPCServer *FriendRPCServer) ApplyToAddFriend(context.Context, *friendPb.ApplyToAddFriendReq) (*friendPb.ApplyToAddFriendResp, error) {
+
+	return nil, status.Errorf(codes.Unimplemented, "method ApplyToAddFriend not implemented")
+}
+func (friendRPCServer *FriendRPCServer) GetPaginationFriendsApplyTo(context.Context, *friendPb.GetPaginationFriendsApplyToReq) (*friendPb.GetPaginationFriendsApplyToResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPaginationFriendsApplyTo not implemented")
+}
+func (friendRPCServer *FriendRPCServer) GetPaginationFriendsApplyFrom(context.Context, *friendPb.GetPaginationFriendsApplyFromReq) (*friendPb.GetPaginationFriendsApplyFromResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPaginationFriendsApplyFrom not implemented")
+}
+func (friendRPCServer *FriendRPCServer) AddBlack(context.Context, *friendPb.AddBlackReq) (*friendPb.AddBlackResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AddBlack not implemented")
+}
+func (friendRPCServer *FriendRPCServer) RemoveBlack(context.Context, *friendPb.RemoveBlackReq) (*friendPb.RemoveBlackResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RemoveBlack not implemented")
+}
+func (friendRPCServer *FriendRPCServer) IsFriend(context.Context, *friendPb.IsFriendReq) (*friendPb.IsFriendResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsFriend not implemented")
+}
+func (friendRPCServer *FriendRPCServer) IsBlack(context.Context, *friendPb.IsBlackReq) (*friendPb.IsBlackResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsBlack not implemented")
+}
+func (friendRPCServer *FriendRPCServer) GetPaginationBlacks(context.Context, *friendPb.GetPaginationBlacksReq) (*friendPb.GetPaginationBlacksResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPaginationBlacks not implemented")
+}
+func (friendRPCServer *FriendRPCServer) DeleteFriend(context.Context, *friendPb.DeleteFriendReq) (*friendPb.DeleteFriendResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteFriend not implemented")
+}
+func (friendRPCServer *FriendRPCServer) RespondFriendApply(context.Context, *friendPb.RespondFriendApplyReq) (*friendPb.RespondFriendApplyResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RespondFriendApply not implemented")
+}
+func (friendRPCServer *FriendRPCServer) SetFriendRemark(context.Context, *friendPb.SetFriendRemarkReq) (*friendPb.SetFriendRemarkResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SetFriendRemark not implemented")
+}
+func (friendRPCServer *FriendRPCServer) GetPaginationFriends(context.Context, *friendPb.GetPaginationFriendsReq) (*friendPb.GetPaginationFriendsResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPaginationFriends not implemented")
+}
+func (friendRPCServer *FriendRPCServer) GetFriendIDs(context.Context, *friendPb.GetFriendIDsReq) (*friendPb.GetFriendIDsResp, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetFriendIDs not implemented")
 }
